9422bc30cd0b48e4562ee3e7b940e294c2f29a4c
Raph <oiu@non.oui>
2023-12-06 08:23:21 +0100

    On essaye

--- Conference-A/ContentView.swift
+++ Conference-A/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "toilet")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("LES TOILETTES")
         }
         .padding()
     }
