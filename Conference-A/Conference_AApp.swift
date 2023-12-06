b9224a792edc98020d1318725e8e105eaa41f7c1
hbolmont <hugo.bolmont@edu.univ-fcomte.fr>
2023-12-06 08:08:52 +0100

    Projet

---
+++ Conference-A/Conference_AApp.swift
@@ -0,0 +1,17 @@
+//
+//  Conference_AApp.swift
+//  Conference-A
+//
+//  Created by bolmont hugo on 06/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Conference_AApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
